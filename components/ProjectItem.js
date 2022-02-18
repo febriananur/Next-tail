2bdf7a495e2e76e3066a01b53a5001c949e05cd0
febrianz <febriananurokhman@gmail.com>
2022-02-18 15:29:57 +0700

    add shadow img project item

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,7 +1,7 @@
 export default function ProjectItem({ name, description, img }) {
   return (
-    <article className="text-center">
-      <img src={img} className="w-full rounded-lg" />
+    <article className="text-center ">
+      <img src={img} className="w-full rounded-lg shadow-lg" />
       <h3 className="text-xl font-semibold mt-4 mb-1">{name}</h3>
       <p className="text-lg text-gray-400">{description}</p>
     </article>
